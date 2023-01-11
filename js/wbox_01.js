705d814035ff7acf57a73b1e0d2be63bbcc02631
dongkyun2331 <dongkyun2331@gmail.com>
2023-01-11 15:05:26 +0900

    a

--- js/wbox_01.js
+++ js/wbox_01.js
@@ -1,3 +1,29 @@
+//background
+$(document).ready(function () {
+  var height = $('.background').height()
+  var num = $('.bul .bli').length
+  var max = height * num
+  var move = 0
+  function background() {
+    move += height
+    $('.bul').animate({ top: -move }, 600, function () {
+      if (move >= max) {
+        $(this).css('top', 0)
+        move = 0
+      }
+    })
+  }
+  backgroundOff = setInterval(background, 3000)
+  $('.bul').append($('.bul .bli').first().clone())
+
+  $('.stop').click(function () {
+    clearInterval(backgroundOff)
+  })
+  $('.start').click(function () {
+    backgroundOff = setInterval(background, 3000)
+  })
+})
+
 // wbox_01
 $(document).ready(function () {
   var height = $('.wbox_01').height()
@@ -13,13 +39,13 @@ $(document).ready(function () {
       }
     })
   }
-  wbox_01Off = setInterval(wbox_01, 2000)
+  wbox_01Off = setInterval(wbox_01, 3000)
   $('.wbox_01ul').append($('.wbox_01ul .wli').first().clone())
 
   $('.stop').click(function () {
     clearInterval(wbox_01Off)
   })
   $('.start').click(function () {
-    wbox_01Off = setInterval(wbox_01, 2000)
+    wbox_01Off = setInterval(wbox_01, 3000)
   })
 })
