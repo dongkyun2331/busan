96f6128ed7c7dfc3d3bf0a4fce26d6222c1c2be1
dongkyun2331 <dongkyun2331@gmail.com>
2023-01-19 17:28:17 +0900

    a

--- js/wbox_01.js
+++ js/wbox_01.js
@@ -1,28 +1,28 @@
 //background
-$(document).ready(function () {
-  var width = $('.background').width()
-  var num = $('.bul .bli').length
-  var max = width * num
-  var move = 0
-  function background() {
-    move += width
-    $('.bul').animate({ left: -move }, 600, function () {
-      if (move >= max) {
-        $(this).css('left', 0)
-        move = 0
-      }
-    })
-  }
-  backgroundOff = setInterval(background, 3000)
-  $('.bul').append($('.bul .bli').first().clone())
+// $(document).ready(function () {
+//   var width = $('.background').width()
+//   var num = $('.bul .bli').length
+//   var max = width * num
+//   var move = 0
+//   function background() {
+//     move += width
+//     $('.bul').animate({ left: -move }, 600, function () {
+//       if (move >= max) {
+//         $(this).css('left', 0)
+//         move = 0
+//       }
+//     })
+//   }
+//   backgroundOff = setInterval(background, 3000)
+//   $('.bul').append($('.bul .bli').first().clone())
 
-  $('.stop').click(function () {
-    clearInterval(backgroundOff)
-  })
-  $('.start').click(function () {
-    backgroundOff = setInterval(background, 3000)
-  })
-})
+//   $('.stop').click(function () {
+//     clearInterval(backgroundOff)
+//   })
+//   $('.start').click(function () {
+//     backgroundOff = setInterval(background, 3000)
+//   })
+// })
 
 // wbox_01
 $(document).ready(function () {
@@ -47,5 +47,12 @@ $(document).ready(function () {
   })
   $('.start').click(function () {
     wbox_01Off = setInterval(wbox_01, 3000)
+  })
+
+  $('.bul').slick({
+    dots: true,
+    autoplay: true,
+    // nextArrow: $('.next'),
+    // prevArrow: $('.prev'),
   })
 })
