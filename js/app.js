19786b05b2cb89f06a802664cf04d63a54d67882
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-17 12:39:15 +0900

    a

--- js/app.js
+++ js/app.js
@@ -1,38 +1,77 @@
-$(document).ready(function () {
-  $('.wbox_01ul').slick({
-    dots: true,
-    autoplay: true,
-    vertical: true,
-    autoplaySpeed: 3000,
-    draggable: false,
-  })
+$('.wbox_01ul').slick({
+  vertical: true,
+  draggable: false,
+  dots: false,
+  arrows: false,
+  pauseOnHover: false,
+  autoplay: true,
+})
 
-  $('.bul').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    nextArrow: $('.right'),
-    prevArrow: $('.left'),
-    draggable: false,
-  })
+$('.bul').slick({
+  draggable: false,
+  dots: false,
+  arrows: false,
+  pauseOnHover: false,
+  autoplay: true,
+})
 
-  $('.right').click(function () {
-    $('.wbox_01ul').slick('slickNext')
-    $('.bul').slick('slickNext')
-  })
+$('.slide_slick').slick({
+  nextArrow: $('.right2'),
+  prevArrow: $('.left2'),
+})
 
-  $('.left').click(function () {
-    $('.wbox_01ul').slick('slickPrev')
-    $('.bul').slick('slickPrev')
-  })
+$('.photo_slick').slick({
+  nextArrow: $('.right3'),
+  prevArrow: $('.left3'),
+})
 
-  $('.start').click(function () {
-    $('.wbox_01ul').slick('slickPlay')
-    $('.bul').slick('slickPlay')
-  })
+$('.photo_slick').on('afterChange', function (event, slick, currentSlide) {
+  $('.number').text(currentSlide + 1)
+})
 
-  $('.stop').click(function () {
-    $('.wbox_01ul').slick('slickPause')
-    $('.bul').slick('slickPause')
-  })
+$('.play3').click(function () {
+  if ($(this).find('i').hasClass('xi-play')) {
+    $('.photo_slick').slick('slickPlay')
+    $(this).find('i').removeClass('xi-play').addClass('xi-pause')
+  } else {
+    $('.photo_slick').slick('slickPause')
+    $(this).find('i').removeClass('xi-pause').addClass('xi-play')
+  }
+})
+
+$('.right').click(function () {
+  $('.wbox_01ul').slick('slickNext')
+  $('.bul').slick('slickNext')
+})
+
+$('.left').click(function () {
+  $('.wbox_01ul').slick('slickPrev')
+  $('.bul').slick('slickPrev')
+})
+
+$('.start').click(function () {
+  $('.wbox_01ul').slick('slickPlay')
+  $('.bul').slick('slickPlay')
+})
+
+$('.stop').click(function () {
+  $('.wbox_01ul').slick('slickPause')
+  $('.bul').slick('slickPause')
+})
+
+$('.y1').magnificPopup({
+  disableOn: 700,
+  type: 'iframe',
+  mainClass: 'mfp-fade',
+  removalDelay: 160,
+  preloader: false,
+
+  fixedContentPos: false,
+})
+
+$('.photo li').hover(function () {
+  $(this).find('.wave').toggleClass('move')
+  $(this).find('.photo_image').toggleClass('move')
+  $(this).find('.photo_p').toggleClass('move')
+  $(this).find('.plus').toggleClass('move')
 })
